840596390e29849fe3f2ca617da25531e0c71f2d
Ryan Stables <id111921@bcud4-010013.students.uce.ac.uk>
2019-04-08 12:40:43 +0100

    first commit

---
+++ seminar1/seminar1/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  seminar1
+//
+//  Created by Ryan Stables on 04/02/2019.
+//  Copyright © 2019 Ryan Stables. All rights reserved.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //    insert code here...
+ 
+    cout << "Hello, \n\t\"Ryan\"!\n" << "\t\tHello, \n\t\t\t" << "Nick!\n";
+
+    return 0;
+}
